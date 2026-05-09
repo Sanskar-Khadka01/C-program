7811b99368a1f19b4cf8b58ce07c9c4188abdb4e
Sanskar Khadka <166985909+Sanskar-Khadka01@users.noreply.github.com>
2026-05-09 19:46:34 +0545

    Update

--- File-handling/Write-Student-Record.c
+++ File-handling/Write-Student-Record.c
@@ -9,13 +9,20 @@ struct Student {
 
 int main() {
     FILE *fp = fopen("students.txt", "w");
-    struct Student s;
-for
-    printf("Enter ID, Name, Marks: ");
-    scanf("%d %s %f", &s.id, s.name, &s.marks);
-
-    fprintf(fp, "%d %s %.2f\n", s.id, s.name, s.marks);
-
+    printf("Enter number of student to store");
+    scanf("%d",&n);
+    struct Student s[n];
+    for(i= 0 ; i< n; i++){
+       printf("Enter student id : ");
+       scanf("%d",&s[i].id);
+       printf("Enter student name : ");
+       scanf("%s",s[i].name);      
+       printf("Enter student  masks : ");
+       scanf("%f",&s[i].masks);
+       
+    
+      fprintf(fp, "%d %s %.2f\n", s[i].id, s[i].name, s[i].marks);
+}
     fclose(fp);
     printf("Record written\n");
     return 0;
