a4ec7a6a184b15c09d477691bf395d348c840838
Sanskar Khadka <166985909+Sanskar-Khadka01@users.noreply.github.com>
2026-05-09 20:37:24 +0545

    Create

---
+++ File-handling/Wap-to-create-and-record-student-record.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+struct Student {
+    int id;
+    char name[50];
+    float marks;
+};
+
+int main() {
+    FILE *fp = fopen("students.txt", "w");
+    int n,i;
+    printf("Enter number of student to store");
+    scanf("%d",&n);
+    struct Student s[n];
+    for(i= 0 ; i< n; i++){
+       printf("Enter student id : ");
+       scanf("%d",&s[i].id);
+       printf("Enter student name : ");
+       scanf("%s",s[i].name);      
+       printf("Enter student  masks : ");
+       scanf("%f",&s[i].masks);
+
+
+      fprintf(fp, "%d %s %.2f\n", s[i].id, s[i].name, s[i].marks);
+}
+    fclose(fp);
+    printf("Record written\n");
+    return 0;
+}
